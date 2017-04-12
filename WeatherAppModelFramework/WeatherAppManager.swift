de7815fa6d1f7a822c2b3bfe4bbb928dc612e764
bell <mukunda.git@gmail.com>
2017-04-11 20:39:32 -0700

    implement weather model and getWeatherReport call

--- WeatherAppModelFramework/WeatherAppManager.swift
+++ WeatherAppModelFramework/WeatherAppManager.swift
@@ -30,10 +30,102 @@ public class WeatherAppManager {
     // get weather report for a city
     public func getWeatherReport(city: String?, success:@escaping (Any)->(), failure:@escaping (Any)->()) {
 
+        let serverURL = getServerURL()
+        let key = getAPIKey()
+        let city = city
+        
+        guard city != nil else {
+            print("Nil search query")
+            return
+        }
+        
+        guard serverURL != nil else {
+            print("Serevr URL is nil")
+            return
+        }
+        
+        guard key != nil else {
+            print("API key is nil")
+            return
+        }
+        
+        let url = serverURL! + city! + "US&APPID=" + key!
+        
+        // FIXME: prepare target url in a nice custom method and return
+        let targetURL = URL(string: url)
+        guard targetURL != nil else {
+            return
+        }
+        
+        print("Out going request: \(targetURL)")
+        
+        let session = URLSession.shared.dataTask(with: targetURL!) {(data, rsponse, error) in
+            guard error == nil else {
+                print("Server error = \(error)")
+                failure(error?.localizedDescription as Any)
+                return
+            }
+            
+            guard let data = data else {
+                print("Data is nil")
+                failure("Data is nil")
+                return
+            }
+            
+            var weatherData: [Weather] = []
+            do {
+                guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+                    print("guard success, returning since errored")
+                    return
+                }
+                
+                print("Incoming response : \(json)")
+                do {
+                    // TODO: check if its 200 response and handle all HTTP code respectively
+                    // For now check if server returned any error message and log accordingly
+                    if let error = json["message"] as? String {
+                        failure(error)
+                        return
+                    }
+                    
+                    // parse json and store it in model
+                    let weather = try Weather.init(json: json)
+                    weatherData.append(weather)
+                    success(weatherData)
+                }
+                catch JSONSerializationError.unknown {
+                    print("Decoding JSON failed with error : \(JSONSerializationError.unknown)")
+                    failure("JSON Serialization failed with unknown key")
+                }
+                catch JSONSerializationError.invalid {
+                    print("Decoding JSON failed with error : \(JSONSerializationError.invalid)")
+                    failure("JSON Serialization failed with Invalid Key.")
+                }
+            }
+            catch {
+                print("JSON Serialization failed.")
+                failure("JSON Serialization failed.")
+            }
+        }
+        session.resume()
     }
     
     // get weather image
     public func getWeatherImage(imageID: String?, success:@escaping (UIImage?)->(), failure:@escaping (Any)->()) {
 
     }
+    
+    // MARK: private methods
+    
+    fileprivate func getServerURL() -> String? {
+        var baseURL = weatherUrl
+        baseURL = baseURL.trimmingCharacters(in: .whitespacesAndNewlines)
+        baseURL = baseURL.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        return baseURL
+    }
+    
+    fileprivate func getAPIKey() -> String? {
+        return API_KEY
+    }
+
 }
