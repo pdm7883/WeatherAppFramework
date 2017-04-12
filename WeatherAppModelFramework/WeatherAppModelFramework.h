53edc0a751848f501e556c1e9589e66de743f3e2
bell <mukunda.git@gmail.com>
2017-04-11 20:26:31 -0700

    Initial Commit

---
+++ WeatherAppModelFramework/WeatherAppModelFramework.h
@@ -0,0 +1,19 @@
+//
+//  WeatherAppModelFramework.h
+//  WeatherAppModelFramework
+//
+//  Created by bell on 4/11/17.
+//  Copyright © 2017 Mukunda Dhirendrachar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeatherAppModelFramework.
+FOUNDATION_EXPORT double WeatherAppModelFrameworkVersionNumber;
+
+//! Project version string for WeatherAppModelFramework.
+FOUNDATION_EXPORT const unsigned char WeatherAppModelFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherAppModelFramework/PublicHeader.h>
+
+
