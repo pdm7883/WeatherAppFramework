cf1bd0cef5e2009e8046a0cb673883304710c849
bell <mukunda.git@gmail.com>
2017-04-11 20:34:20 -0700

    initial commit

--- WeatherAppModelFramework/WeatherAppManager.swift
+++ WeatherAppModelFramework/WeatherAppManager.swift
@@ -21,4 +21,19 @@ public class WeatherAppManager {
     public init () {
         
     }
+    
+    // FIXME: Read it from plist file: Store safely
+    fileprivate let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q="
+    fileprivate let baseImageUrl = "http://openweathermap.org/img/w/"
+    fileprivate let API_KEY = "3da5c0ce08da71e8fc86e78af4e17804"
+    
+    // get weather report for a city
+    public func getWeatherReport(city: String?, success:@escaping (Any)->(), failure:@escaping (Any)->()) {
+
+    }
+    
+    // get weather image
+    public func getWeatherImage(imageID: String?, success:@escaping (UIImage?)->(), failure:@escaping (Any)->()) {
+
+    }
 }
